3804458a5c48d91be4c3ad8243caee247cfe7cfd
sanaeas <sanaassim1@gmail.com>
2022-12-04 10:21:22 +0100

    config firebase functions

--- functions/index.js
+++ functions/index.js
@@ -15,20 +15,21 @@ app.use(express.json());
 // - API routes
 app.get('/', (request, response) => response.status(200).send('Hello World'));
 
-app.post('/payments/create', async (request, response) => {
+app.post("/payments/create", async (request, response) => {
     const total = request.query.total;
-
-    console.log('Payment request received for this amount >>> ', total);
-
+  
+    console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+  
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: total,
-        currency: 'usd',
+      amount: total, // subunits of the currency
+      currency: "usd",
     });
-
+  
+    // OK - Created
     response.status(201).send({
-        clientSecret: paymentIntent.client_secret,
-    })
-})
+      clientSecret: paymentIntent.client_secret,
+    });
+});
 
 // - Listen command
-exports.api = functions.https.onRequest(app)
+exports.api = functions.https.onRequest(app);
